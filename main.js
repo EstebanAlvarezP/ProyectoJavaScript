270941379c388d770074b403e2329827cb8716da
Esteban Alvarez <esteban.yesid2255@gmail.com>
2023-10-28 00:47:47 -0500

    cambios en el codigo

--- main.js
+++ main.js
@@ -1,55 +1,119 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const productItems = document.querySelectorAll("#product-list li");
-  const cart = document.querySelector("#cart");
-  const total = document.querySelector("#total");
+// Array de productos
+const products = [
+    { id: 1, name: 'Hamburguesa', price: 20.00 },
+    { id: 2, name: 'HotDog', price: 10.00 },
+    { id: 3, name: 'Nachos', price: 25.00 },
+    { id: 4, name: 'Pizza', price: 30.00 },
+];
 
-  let cartItems = [];
+// Carrito de compras
+const cart = {
+    items: [],
+    total: 0.00,
+};
 
-  function updateCart() {
-      cart.innerHTML = "";
-      let cartTotal = 0;
+// Función para mostrar la lista de productos
+function displayProducts() {
+    const productList = document.getElementById('product-list');
+    productList.innerHTML = '';
+    products.forEach(product => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${product.name} - $${product.price} <button onclick="addToCart(${product.id})">Agregar al carrito</button>`;
+        productList.appendChild(listItem);
+    });
+}
 
-      cartItems.forEach((item) => {
-          const li = document.createElement("li");
-          li.textContent = `${item.name} - $${item.price.toFixed(2)}`;
-          const removeButton = document.createElement("button");
-          removeButton.textContent = "Eliminar";
-          removeButton.addEventListener("click", () => removeFromCart(item));
-          li.appendChild(removeButton);
-          cart.appendChild(li);
-          cartTotal += item.price;
-      });
+// Función para agregar un producto al carrito
+function addToCart(productId) {
+    const product = products.find(p => p.id === productId);
+    if (product) {
+        cart.items.push(product);
+        cart.total += product.price;
+        updateCartDisplay();
+    }
+}
 
-      total.textContent = `Total: $${cartTotal.toFixed(2)}`;
-  }
+// Función para actualizar la interfaz de usuario del carrito
+function updateCartDisplay() {
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    cartItems.innerHTML = '';
+    cart.items.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${item.name} - $${item.price}`;
+        cartItems.appendChild(listItem);
+    });
+    cartTotal.innerText = cart.total.toFixed(2);
+}
+//Sweet alert para los botones de pedir y borrar
+document.getElementById("boton").addEventListener("click", ()=>{
+    Swal.fire({
+        icon: 'success',
+        title: '¡Pedido creado con éxito!',
+      })
+})
+document.getElementById("clear-cart-button").addEventListener("click", ()=>{
+    Swal.fire({
+        icon: 'error',
+        title: 'Tus productos fueron eliminados',
+        text: 'Puedes volver a añadir tus productos al carrito',
+      })
+})
 
-  function addToCart(product) {
-      cartItems.push(product);
-      updateCart();
-  }
+//funcion para cargar datos desde el archivo JSON y mostrarlos en el html
+fetch('productos.json')
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            console.log(data);
+            renderProductos(data)
 
-  function removeFromCart(product) {
-      const index = cartItems.indexOf(product);
-      if (index !== -1) {
-          cartItems.splice(index, 1);
-          updateCart();
-      }
-  }
+        })
+        .catch(error => console.error("Error al cargar productos: " + error));
 
-  fetch("URL_DE_TU_API_DE_PRODUCTOS")
-  .then((response) => response.json())
-  .then((products) => {
-      products.forEach((product) => {
-          const li = document.createElement("li");
-          li.textContent = `${product.name} - $${product.price.toFixed(2)}`;
-          const addToCartButton = document.createElement("button");
-          addToCartButton.textContent = "Agregar al Carrito";
-          addToCartButton.addEventListener("click", () => addToCart(product));
-          li.appendChild(addToCartButton);
-          productContainer.appendChild(li);
-      });
-  })
-  .catch((error) => {
-      console.error("Error al cargar los productos:", error);
-  });
+        function renderProductos(productos) {
+            const contenido = document.querySelector('#lista');
+            let html = "";
+        
+            for (let i = 0; i < productos.length; i++) {
+                const producto = productos[i];
+                if (i % 2 === 0) {
+                    html += '<div class="row">';
+                }
+        
+                html += `
+                <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                 <img src="./img/${producto.img}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                   <h5 class="card-title">${producto.nombre}</h5>
+                   <p class="card-text">${producto.ingredientes}</p>
+                   <p class="card-cost">$${producto.precio}</p>
+                   <a href="#" class="btn btn-primary"onclick="addToCart(${producto.id})">Agregar al carrito</a>
+                 </div>
+                </div>
+               </div>
+                `;
+        
+                if (i % 2 !== 0 || i === productos.length - 1) {
+                    html += '</div>';
+                }
+            }
+        
+            contenido.innerHTML = html;
+        }
+
+        const clearCartButton = document.getElementById('clear-cart-button');
+
+clearCartButton.addEventListener('click', function () {
+
+    cart.items = []; 
+    cart.total = 0.00; 
+    updateCartDisplay(); 
 });
+
+// Carga inicial de productos y muestra del carrito
+displayProducts();
+updateCartDisplay();
+
